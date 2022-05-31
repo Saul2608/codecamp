5180d47f08782185ba8ed423008831587d735247
Saul <saulmckenzie2608@gmail.com>
2022-05-31 11:26:15 +1000

    Web test to click on form and planets in sequence. Submit form test which inputs information into form, submits and then asserts that message is received.

--- src/test/java/apitests/WebTestSuite.java
+++ src/test/java/apitests/WebTestSuite.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -47,6 +48,32 @@ public class WebTestSuite {
         WebElement planetsMenuButton = driver.findElement(By.cssSelector("[aria-label=planets]"));
         planetsMenuButton.click();
         Assertions.assertEquals("https://d18u5zoaatmpxx.cloudfront.net/#/planets",driver.getCurrentUrl());
+
     }
 
+    @Test
+    public void FormsInsertTest(){
+
+        WebElement formsInsertField = driver.findElement(By.cssSelector("[aria-label=forms]"));
+        formsInsertField.click();
+        var insertName = driver.findElement(By.id("name"));
+                insertName.sendKeys("Saul");
+        var insertEmail = driver.findElement(By.id("email"));
+                insertEmail.sendKeys("Example@email.com");
+        var insertState = driver.findElement(By.id("state"));
+                insertState.sendKeys("NSW");
+        var agreeButton = driver.findElement(By.cssSelector("[for=agree]"));
+                agreeButton.click();
+        var Buttons = driver.findElements(By.className("v-btn"));
+        for (WebElement button : Buttons){
+
+                if (button.getText().equalsIgnoreCase("submit")){
+                button.click();
+            }
+        }
+
+        var popupMessage = driver.findElement(By.className("popup-message"));
+        new WebDriverWait(driver, 10).until(d ->popupMessage.isDisplayed());
+        Assertions.assertEquals("Thanks for your feedback Saul",popupMessage.getText());
+    }
 }
